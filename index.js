26316496ff2fa814df5ce3ee58dbcbc71068adf5
0c0de <gamesfreakin@gmail.com>
2018-10-03 21:31:44 +0200

    Added actor info and actor search

--- index.js
+++ index.js
@@ -31,8 +31,45 @@ function getFull(id) {
     return { ...data[0], ...data[1], ...data[2] }
   }).catch(ifError)
 }
+
+function getActor(id){
+  return request(`https://www.imdb.com/name/${id}/?ref_=tt_cl_t1`).then((data) => {
+    const $ = cheerio.load(data);
+    let result = [];
+    let info = $('div.inline').text().split('\n').join(' ').split('...');
+    let birthDate = $('div#name-born-info a:nth-child(1)').text();
+    let birthYear = $('div#name-born-info a:nth-child(2)').text();
+    let bornInfo = $('div#name-born-info a:nth-child(3)').text();
+    let name = $('h1.header span.itemprop').text();
+    let image = $('a img#name-poster').attr('src');
+    result.push({
+      actorName: name,
+      actorImage: image,
+      actorInfo: info[0].trim(),
+      actorBirth: birthDate + ", " + birthYear,
+      actorBorn: bornInfo
+    });
+    return result;
+  }).catch(ifError)
+}
+
+function searchActor(name){
+  return request(`https://www.imdb.com/find?ref_=nv_sr_fn&q=${name}&s=nm`).then((data) => {
+    const $ = cheerio.load(data);
+    let result = [];
+    $('tr').each((index, element) => {
+      let image = $(element).find('td.primary_photo > a > img').attr('src');
+      let name = $(element).find('td.result_text > a').text();
+      result.push({
+        actorImage: image,
+        actorName: name
+      });
+    });
+    return result;
+  }).catch(ifError)
+}
 // getTrendingGenre('comedy', 7).then((data)=>{
 //   console.log(data)
 // })
 
-module.exports = { scrapper, getTrendingGenre, getTrending, search, getFull, awardsPage, getCast, simpleSearch, ifError, request }
+module.exports = {scrapper,searchActor ,getActor, getTrendingGenre, getTrending, search, getFull, awardsPage, getCast, simpleSearch, ifError}
